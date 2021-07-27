58b82549dc057cad958f9b82284c7c3ec694156c
nabu0305 <66404021+nabu0305@users.noreply.github.com>
2021-07-27 19:46:34 +0900

    javascript add white/black btn

---
+++ phpjs/1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+   <script>
+     password = prompt("비밀번호");
+     if(password == 123){
+         document.write("반갑습니다")
+     }
+   </script>
+</body>
+</html>
